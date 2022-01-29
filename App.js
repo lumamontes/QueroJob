8f84d6c6d08232a69e88a88564e09b6fe1dc6006
Luma Montes <lumagoesmontes@gmail.com>
2022-01-28 22:59:42 -0300

    iniciado estrutura de  stack navigation em app

--- App.js
+++ App.js
@@ -1,78 +1,38 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  Poppins_400Regular,
-  Poppins_700Bold,
-} from '@expo-google-fonts/poppins'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {
-  Rubik_700Bold,
-} from '@expo-google-fonts/rubik'
+import Landing from './src/screens/Landing';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+const Stack = createNativeStackNavigator();
 
-export default function App() {
-  let [fontsLoaded] = useFonts({
-    Poppins_700Bold,
-    Poppins_400Regular,
-    Rubik_700Bold
-  });
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.heroImage}
-          source={{
-            uri: 'https://cdn.pixabay.com/photo/2018/09/25/11/45/pixel-cells-3702056_960_720.png ',
+function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen
+            name="Landing"
+            component={Landing}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="Login" component={Login}
+          options={{
+            headerShown: false,
           }}
-        />
-        <View>
-          <Text style={{ color: '#4E9F3D', fontSize: 36, fontFamily: 'Poppins_700Bold' }} >QueroJob</Text>
-          <Text style={{ color: '#1E5128', fontSize: 48, fontFamily: 'Rubik_700Bold' }} >Trilhe seu futuro profissional</Text>
-        </View>
-          <TouchableOpacity style={styles.startButton}>
-            <Text style={{ color: 'white', fontSize: 26, fontFamily: 'Poppins_400Regular' }}>Começar</Text>
-            <AntDesign name="right" size={24} color="#F1F1F1" />
-          </TouchableOpacity>
-      </View>
-    );
-  }
+          />
+          <Stack.Screen name="Register" component={Register}
+          />
+          {/* <Stack.Screen name="Rankings" component={Rankings} /> */}
+        </Stack.Navigator>
+
+      </NavigationContainer> 
+    </>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#F1F1F1',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  heroImage: {
-    width: 300,
-    height: 200,
-  },
-  startButton: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#1E5128',
-    fontWeight: 'bold',
-    fontSize: 24,
-    borderRadius: 30,
-    width: 302,
-    height: 67,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  }
-});
+export default App;
---
+++ src/screens/Register/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+export default function Register({ navigation }) {
+    return (
+      <View style={styles.container}>
+          <Text>Register</Text>
+      </View>
+    );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#F1F1F1',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  }
+})

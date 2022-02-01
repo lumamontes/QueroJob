a4ac8a211d9b0144bc94b506b418dba678f36fe0
Luma Montes <lumagoesmontes@gmail.com>
2022-01-31 23:42:18 -0300

    iniciado tela de trilhas

---
+++ src/components/TrilhaCard/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { AntDesign} from '@expo/vector-icons';
+import * as Progress from 'react-native-progress';
+
+export default function TrilhaCard(props) {
+  return (
+    <TouchableOpacity style={[styles.container,{backgroundColor: `${props.cardcolor}` }]}>
+      <View>
+        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.subtitle}>{props.aulas} aulas</Text>
+
+        <Progress.Bar progress={props.progress} width={200} height={3} color={'#F9F6F6'} unfilledColor={'#C4C4C4'} borderColor={'transparent'}/>
+      </View>
+      <View>
+        <AntDesign name="right" size={24} color="#FFFFFF" />
+      </View>
+    </TouchableOpacity>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 120,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+    borderRadius: 8,    
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    
+    elevation: 4,
+  },
+  title: {
+    fontSize: 18,
+    color: '#F1F1F1',
+    fontFamily: 'Roboto_700Bold'
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#D2D2D2',
+    fontFamily: 'Roboto_400Regular',
+    marginBottom: 10,
+  }
+});
--- src/screens/TrilhaTeorica/index.js
+++ src/screens/TrilhaTeorica/index.js
@@ -1,19 +1,80 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, FlatList } from 'react-native';
+import TrilhaCard from '../../components/TrilhaCard';
+
 
 export default function TrilhaTeorica({ navigation }) {
+    const Options = [
+        {
+            id: '1',
+            title: 'Área',
+            aulas: '1',
+            progress: 0.9,
+            cardcolor: '#B66666'
+        },
+        {
+            id: '2',
+            title: 'Currículo',
+            aulas: '12',
+            progress: 0.4,
+            cardcolor: '#3F8FA8'
+        },
+        {
+            id: '3',
+            title: 'Vagas',
+            aulas: '10',
+            progress: 0.6,
+            cardcolor: '#267B19'
+        },
+        {
+            id: '4',
+            title: 'Portfólio',
+            aulas: '10',
+            progress: 0.6,
+            cardcolor: '#B66666'
+        },
+    ];
     return (
-      <View style={styles.container}>
-          <Text>Trilha teórica</Text>
-      </View>
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.title}>Trilhas</Text>
+                <Text style={styles.area_title}>Desenvolvimento</Text>
+            </View>
+            <View style={styles.options_container}>
+                <FlatList
+                    data={Options}
+                    keyExtractor={option => String(option.id)}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item: option }) =>
+                    (
+                        <TrilhaCard title={option.title} progress={option.progress} aulas={option.aulas} cardcolor={option.cardcolor}/>
+                    )}
+                >
+                </FlatList> 
+            </View>
+        </View>
     );
-  }
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#F1F1F1',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#F1F1F1',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    title: {
+        fontSize: 20,
+        color: '#727272',
+        marginBottom: 20,
+        fontFamily: 'Roboto_700Bold'
+    },
+    area_title: {
+        fontSize: 18,
+        color: '#4E9F3D',
+        fontFamily: 'Roboto_400Regular'
+
+    },
+    options_container: {
+        width: '80%',
+    }
 });

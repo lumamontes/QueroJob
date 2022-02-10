c86943a794e2c75dee2f2d8245e75f8cc763e172
Luma Montes <lumagoesmontes@gmail.com>
2022-02-10 13:25:13 -0300

    adicionado conteudo

--- src/components/MenuTeorico/index.js
+++ src/components/MenuTeorico/index.js
@@ -1,26 +1,85 @@
 
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Animated, TouchableOpacity, FlatList, Dimensions } from 'react-native';
-const {width, height} = Dimensions.get('screen');
+const { width, height } = Dimensions.get('screen');
 
 export default function MenuTeorico(props) {
+    const habilidades = [{
+        nome: 'Conhecimento de lógica de programação'
+    }, {
+        nome: 'Analisar e resolver problemas'
+
+    }, {
+        nome: 'Atualizar-se e estudar constantemente'
+
+    }, {
+        nome: 'Compreender diferentes tipos de linguagem de programação'
+
+    }, {
+        nome: 'Inglês'
+
+    }
+    ]
+
+    const cargos = [{
+        cargo: 'Desenvolvedor Web'
+    }, {
+        cargo: 'Desenvolvedor Android'
+
+    }, {
+        cargo: 'Desevolvendor FullStrack'
+
+    }, {
+        cargo: 'Desenvolvedor Mobile'
+
+    }
+
+    ]
+
     return (
         <View style={styles.container}>
             <View>
                 <Text style={styles.title}> Desenvolvimento</Text>
-                <Text style={styles.content}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <Text style={styles.content}>
+                    A área de Desenvolvimento foca na construção de sites, aplicativos, softwares, bancos de dados e quaisquer páginas que podemos encontrar e construir na internet hoje, das mais simples até as mais complexas.</Text>
             </View>
             <View>
                 <Text style={styles.title}> Cargos</Text>
-                <Text style={styles.content}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <FlatList
+                    data={cargos}
+                    keyExtractor={option => String(cargos.cargo)}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item: option }) =>
+                    (
+                        <Text style={styles.content}>{option.cargo}</Text>
+
+
+                    )}
+                >
+                </FlatList> 
             </View>
             <View>
                 <Text style={styles.title}>Faixa Salarial</Text>
-                <Text style={styles.content}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <Text style={styles.content}>
+                    Desenvolvedor Web é de R$ 2.949 (em média)</Text>
             </View>
             <View>
                 <Text style={styles.title}>Habilidades buscadas</Text>
-                <Text style={styles.content}> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                <FlatList
+                    data={habilidades}
+                    keyExtractor={option => String(habilidades.nome)}
+                    showsVerticalScrollIndicator={false}
+                    renderItem={({ item: option }) =>
+                    (
+                        <Text style={styles.content}>{option.nome}</Text>
+
+
+                    )}
+                >
+                </FlatList> 
+                    
+                    
+                    
             </View>
         </View>
     )
@@ -40,7 +99,7 @@ const styles = StyleSheet.create({
     },
     content: {
         color: '#727272',
-        fontSize:18,
+        fontSize: 18,
         fontFamily: 'Roboto_400Regular'
     }
 });
